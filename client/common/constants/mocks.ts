b51a445b2d550b561f7b97aaa100938346febf41
Cvesperova <klaracvesperova@gmail.com>
2020-10-24 19:11:44 +0200

    feat: Add mocks

--- client/common/constants/mocks.ts
+++ client/common/constants/mocks.ts
@@ -47,9 +47,9 @@ export const mockedJewelry = [
   },
   {
     _id: '3',
-    name: 'Prsten s ametystem a diamanty 01 33',
+    name: 'Prsten s ametystem a diamanty 01',
     price: 49510,
-    image: '/static/jewelry/pendants/diamond_33.jpg',
+    image: '/static/jewelry/rings/ametyst_01.jpg',
     material: '14k bílé zlato 585/1000',
     netWeight: 3.83,
     gem: [
@@ -66,7 +66,7 @@ export const mockedJewelry = [
         type: 'přírodní DIAMANT',
         quantity: 36,
         cut: 'kulatý briliantový',
-        color: 'vyácně bílý (F-G-H)',
+        color: 'vzácně bílý (F-G-H)',
         clarity: 'VS',
         weight: 0.371,
         diameter: '1.35-1.45'
@@ -75,9 +75,9 @@ export const mockedJewelry = [
   },
   {
     _id: '4',
-    name: 'Naušnice se žlutými diamanty 01 33',
+    name: 'Naušnice se žlutými diamanty 01',
     price: 77270,
-    image: '/static/jewelry/pendants/diamond_33.jpg',
+    image: '/static/jewelry/earings/yellow_diamond_01.jpg',
     material: '14k bílé a žluté zlato 585/1000',
     netWeight: 2.55,
     gem: [
@@ -86,18 +86,95 @@ export const mockedJewelry = [
         quantity: 2,
         cut: 'kulatý briliant',
         color: 'neurčen',
-        clarity: 'VS-SI1-SI2,
+        clarity: 'VS-SI1-SI2',
         weight: 0.129,
         diameter: '2.5'
       },
       {
         type: 'přírodní DIAMANT',
-        quantity: 36,
-        cut: 'kulatý briliantový',
-        color: 'vyácně bílý (F-G-H)',
+        quantity: 16,
+        cut: 'kulatý briliant',
+        color: 'neurčen',
+        clarity: 'VS-SI1-SI2',
+        weight: 0.181,
+        diameter: '1.35-1.4'
+      },
+      {
+        type: 'přírodní DIAMANT',
+        quantity: 2,
+        cut: 'kulatý briliant',
+        color: 'výjimečně bílý (D-E)',
         clarity: 'VS',
-        weight: 0.371,
-        diameter: '1.35-1.45'
+        weight: 0.260,
+        diameter: '3.25'
+      },
+      {
+        type: 'přírodní DIAMANT',
+        quantity: 2,
+        cut: 'kulatý briliant',
+        color: 'výjimečně bílý (D-E)',
+        clarity: 'VS',
+        weight: 0.108,
+        diameter: '2.4'
+      }
+    ]
+  },
+  {
+    _id: '5',
+    name: 'Naušnice se smaragdy a diamanty',
+    price: 118270,
+    image: '/static/jewelry/earings/smaragd.jpg',
+    material: '14k bílé zlato 585/1000',
+    netWeight: 3.97,
+    gem: [
+      {
+        type: 'přírodní BERYL-SMARAGD',
+        quantity: 2,
+        cut: 'osmihran stupňový',
+        color: 'lehce namodrale zelený',
+        clarity: 'transparentní',
+        weight: 0.894,
+        diameter: '6x4',
+        modification: 'výplň fisur (F1-F2)',
+        origin: 'Kolumbie'
+      },
+      {
+        type: 'přírodní DIAMANT',
+        quantity: 32,
+        cut: 'kulatý briliantový',
+        color: 'vzácně bílý (D-E-F)',
+        clarity: 'VS',
+        weight: 0.282,
+        diameter: '1.25-1,45'
+      }
+    ]
+  },
+  {
+    _id: '6',
+    name: 'Přívěsek s tanzanitem a diamantem 01',
+    price: 10740,
+    image: '/static/jewelry/pendants/tanzanit_01.jpg',
+    material: '14k bílé zlato 585/1000',
+    netWeight: 0.59,
+    gem: [
+      {
+        type: 'přírodní ZOISIT-TANZANIT',
+        quantity: 1,
+        cut: 'pantlok fasetovaný',
+        color: 'nafialovělo modrý',
+        clarity: 'transparentní',
+        weight: 0.458,
+        diameter: '6x4'
+      },
+      {
+        type: 'přírodní DIAMANT',
+        quantity: 1,
+        cut: 'kulatý briliantový',
+        color: 'výjimečně bílý (E)',
+        clarity: 'VS',
+        weight: 0.024,
+        diameter: '1.8'
       }
     ]
   }
+]
