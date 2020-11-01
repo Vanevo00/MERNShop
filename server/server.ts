1ee00efd6dc73734feed16acb3d927d3fb73871f
Vojtech Vanek <vanevo00@gmail.com>
2020-11-01 15:23:54 +0100

    feat: initialize ApolloGraphQL

--- server/server.ts
+++ server/server.ts
@@ -1,8 +1,38 @@
 import express from 'express'
 import cors from 'cors'
+import { ApolloServer, gql } from 'apollo-server-express'
 
+const typeDefs = gql`    
+    type Book {
+        title: String
+        author: String
+    }
+    
+    type Query {
+        books: [Book]
+    }
+`
 
+const books = [
+  {
+    title: 'The Awakening',
+    author: 'Kate Chopin',
+  },
+  {
+    title: 'City of Glass',
+    author: 'Paul Auster',
+  },
+]
+
+const resolvers = {
+  Query: {
+    books: () => books,
+  },
+}
+
+const server = new ApolloServer({ typeDefs, resolvers })
 const app = express()
+server.applyMiddleware({ app })
 const port = 2999
 
 app.use(express.json()) // to accept body data
